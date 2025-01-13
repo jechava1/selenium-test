e16a6d61057be62bbe160a2bc8ff3998abc5fb12
jechava <jechava@xyz.com>
2025-01-12 23:50:18 -0600

    capstone test updates

--- src/main/java/org/test/my_app_test/App.java
+++ src/main/java/org/test/my_app_test/App.java
@@ -1,49 +1,60 @@
 package org.test.my_app_test;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args ) throws InterruptedException
+public class App {
+    public static void main( String[] args ) throws InterruptedException, IOException
     {
         System.out.println( "Hello World!" );
         
+        ChromeOptions options= new ChromeOptions();
+        options.addArguments("--headless");
         WebDriver driver = new ChromeDriver();
+        //open URL from test server
+        // create arguments to fill out the form on the webpage
+        //run mvn clean package assembly:single
 
-        driver.get("https://www.facebook.com/");
+        driver.get("http://54.173.179.159:8081/contact.html");
 
-        driver.getTitle();
+//        driver.getTitle();
 
-        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(1000));
+        
+        driver.findElement(By.name("your_name")).sendKeys("Jennifer");
+        driver.findElement(By.name("phone_number")).sendKeys("7132564458");
+        driver.findElement(By.name("email_address")).sendKeys("Jechava1@gmail.com");
+        driver.findElement(By.name("your_message")).sendKeys("Hi, How are you?");
+        driver.findElement(By.id("my-button")).click();
+        
+        String message= driver.findElement(By.id("response")).getText();
+        
+        System.out.println(message);
 
-        WebElement username = driver.findElement(By.name("email"));
-        WebElement password=driver.findElement(By.name("pass"));
-
-        WebElement submitButton = driver.findElement(By.cssSelector("button"));
-        Thread.sleep(2000);
-
-        username.sendKeys("jechava1");
-        Thread.sleep(2000);
-        password.sendKeys("password");
-        Thread.sleep(1000);
-
-        Thread.sleep(1000);
-        submitButton.click();
-        Thread.sleep(2000);
-
-        WebElement message = driver.findElement(By.id("message"));
-       // message.getText();
-        System.out.println(message.getText());
-
+        //capture screenshot
+        TakesScreenshot scrShot = ((TakesScreenshot)driver);
+        
+        File srcFile=scrShot.getScreenshotAs(OutputType.FILE);
+        
+        File destFile= new File("test-report.jpg");
+        
+        FileUtils.copyFile(srcFile,destFile);
+        
         driver.quit();
+        
+        System.out.println("Script executed and results captured");
     }
 }
