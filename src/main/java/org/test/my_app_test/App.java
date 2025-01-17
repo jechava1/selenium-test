2ca540d2f7c343338264a1e51336bc787982c668
jechava1 <jenniferechavarria01@gmail.com>
2025-01-17 00:57:23 -0600

    Update App.java
    chrome driver issue

--- src/main/java/org/test/my_app_test/App.java
+++ src/main/java/org/test/my_app_test/App.java
@@ -20,7 +20,7 @@ public class App {
     public static void main( String[] args ) throws InterruptedException, IOException
     {
         System.out.println( "Hello World!" );
-        
+        WebDriverManager.chromedriver().clearDriverCache().setup();
         ChromeOptions options= new ChromeOptions();
         options.addArguments("--headless");
         WebDriver driver = new ChromeDriver();
